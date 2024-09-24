16c55811a94ae07834c01ae269676801ee64d94b
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-09-24 17:32:15 -0500

    Using HTTP in api

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ $currentWiki = $currentLang['wiki'];
 $wikiproject = $currentWiki === "globalwiki" ? "all" : $currentWiki;
 
 // Hacer la solicitud a la API (sin dominio)
-$response = file_get_contents("https://wikipeoplestats.toolforge.org/api/stats.php?project=$wikiproject");
+$response = file_get_contents("http://wikipeoplestats.toolforge.org/api/stats.php?project=$wikiproject");
 $data = json_decode($response, true);
 
 // Verificar si hay un error en la respuesta
