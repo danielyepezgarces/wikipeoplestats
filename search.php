5b9856649be96c5356d834994853b04da4ced1f7
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-10-01 17:15:39 -0500

    Adding TailwindCSS

--- search.php
+++ search.php
@@ -8,6 +8,19 @@ include 'languages.php'; // Cargar idiomas y traducciones
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo __('sitename'); ?></title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    colors: {
+                        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+                    }
+                }
+            }
+        }
+    </script>
     <!-- Otras cabeceras -->
 </head>
 <body>
