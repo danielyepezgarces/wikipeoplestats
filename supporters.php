3c36f0722204d03b90d55057a832c26a575dd031
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-10-23 19:34:36 -0500

    Update supporters.php

--- supporters.php
+++ supporters.php
@@ -2,7 +2,7 @@
     <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
         <div class="mb-4 flex flex-col items-center md:mb-8 lg:flex-row lg:justify-between">
             <h2 class="mb-2 text-center text-2xl font-bold text-gray-800 dark:text-white lg:mb-0 lg:text-3xl"><?php echo __('trusted_by_wikimedia_chapters'); ?></h2>
-            <p class="max-w-md text-center text-gray-400 lg:text-right"><?php echo __('wikimedia_chapters_trust_tool'); ?></p>
+            <p class="max-w-md text-center text-gray-400 dark:text-white lg:text-right"><?php echo __('wikimedia_chapters_trust_tool'); ?></p>
         </div>
 
         <div class="grid grid-cols-2 gap-4 rounded-lg md:grid-cols-3 lg:gap-6">
