3eb19faf2389f08c0f8a29b3c1a6cce82b5b1fc6
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-11-27 23:40:35 -0500

    Update rankingswiki.php

--- rankingswiki.php
+++ rankingswiki.php
@@ -42,6 +42,7 @@ $dataArray = array_values($data);
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
     <!-- Incluir JS de DataTables -->
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.tailwindcss.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
