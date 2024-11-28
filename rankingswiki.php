eed1c42859bb228e880b90a59f68b0074077b185
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-11-28 00:49:36 -0500

    Update rankingswiki.php

--- rankingswiki.php
+++ rankingswiki.php
@@ -221,6 +221,14 @@ $(document).ready(function() {
             // Mejorar la apariencia del input de búsqueda
             $('.dataTables_wrapper .dataTables_filter input').addClass('ml-2');
             $('.dataTables_wrapper .dataTables_filter label').addClass('flex items-center');
+
+            $('table.dataTable thead th').removeClass('sorting sorting_asc sorting_desc')
+            .addClass('sorting bg-gray-100 dark:bg-gray-700 text-gray-700 dark:text-gray-300 font-medium text-sm py-3 px-4 border-b border-gray-300 dark:border-gray-600');
+
+           //  Personalizar íconos de orden
+           $('table.dataTable thead th.sorting').append('<span class="ml-2">↕</span>'); // Ícono para ordenar
+           $('table.dataTable thead th.sorting_asc').append('<span class="ml-2">↑</span>'); // Orden ascendente
+           $('table.dataTable thead th.sorting_desc').append('<span class="ml-2">↓</span>'); // Orden descendente
         }
     });
 });
