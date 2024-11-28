511a83c6ae157896bd6f21f67d0889b9aabe23a3
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-11-27 23:19:33 -0500

    Update rankingswiki.php

--- rankingswiki.php
+++ rankingswiki.php
@@ -34,12 +34,16 @@ $dataArray = array_values($data);
     <title><?php echo __('sitename'); ?></title>
     <link href='https://tools-static.wmflabs.org/fontcdn/css?family=Montserrat:700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://tools-static.wmflabs.org/cdnjs/ajax/libs/font-awesome/6.6.0/css/all.css">
-    <link href="https://cdn.datatables.net/v/bs5/dt-2.1.8/b-3.2.0/b-html5-3.2.0/datatables.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/v/bm/dt-2.1.8/b-3.2.0/b-html5-3.2.0/datatables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://tools-static.wmflabs.org/cdnjs/ajax/libs/odometer.js/0.4.8/themes/odometer-theme-minimal.min.css">
     <!-- Incluir CSS de DataTables -->
-    <script src="https://cdn.datatables.net/v/bs5/dt-2.1.8/b-3.2.0/b-html5-3.2.0/datatables.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 
+    <!-- Incluir jQuery (DataTables depende de jQuery) -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Incluir JS de DataTables -->
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
