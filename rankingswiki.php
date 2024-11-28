65937ac38fc8a7fe2467a7db1d36fe07c3d770f2
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2024-11-27 22:30:00 -0500

    Update rankingswiki.php

--- rankingswiki.php
+++ rankingswiki.php
@@ -158,10 +158,11 @@ $(document).ready(function() {
         "data": tableData,          // Pasa todos los datos desde PHP
         "columns": [
             {
+                "data": null, 
                 "render": function (data, type, row, meta) {
                     return meta.row + 1;  // Esto muestra el índice de la fila
                 }
-            }, // Columna # (índice de fila)
+            },
             { "data": "site" },            // Columna "Project"
             { "data": "totalPeople" },     // Columna "Total People"
             { "data": "totalWomen" },      // Columna "Total Women"
