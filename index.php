fe33bbadb7c8e7b4a6469f7905e9c50b5ff3e746
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2025-01-11 12:13:59 -0500

    Removing author for SEO changes

--- index.php
+++ index.php
@@ -75,7 +75,6 @@ $message = sprintf(__('main_home_content'), $currentProjectTranslated);
     <title><?php echo __('sitename'); ?></title>
     <meta name="description" content="<?php echo __('site_description'); ?>">
     <meta name="keywords" content="<?php echo __('site_keywords'); ?>">
-    <meta name="author" content="Daniel Yepez Garces">
     <link href='https://tools-static.wmflabs.org/fontcdn/css?family=Montserrat:700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="libs/font-awesome/all.min.css">
     <link rel="stylesheet" href="https://tools-static.wmflabs.org/cdnjs/ajax/libs/odometer.js/0.4.8/themes/odometer-theme-minimal.min.css">
