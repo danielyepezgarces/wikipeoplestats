f288d43e428be826cee9895c551823006f913d05
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2025-01-15 00:50:53 -0500

    Update rankingswiki.php

--- rankingswiki.php
+++ rankingswiki.php
@@ -4,8 +4,6 @@ include 'languages.php';
 function fetchData($timeFrame, $projectGroup) {
     $url = "https://api.wikipeoplestats.org/v1/rankings/$projectGroup/$timeFrame";
 
-    echo $url;
-
     // Usar file_get_contents o cURL para obtener los datos de la API
     $response = file_get_contents($url);
 
