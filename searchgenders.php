00f53dd1d74d2a80583d05cce0a9591d76e71857
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2025-01-23 19:35:34 -0500

    Update searchgenders.php

--- searchgenders.php
+++ searchgenders.php
@@ -8,9 +8,9 @@ include 'languages.php'; // Cargar idiomas y traducciones
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo __('sitename'); ?></title>
-   <link href='assets\css\fonts\styles.css' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <script src="https://www.wikipeoplestats.org/assets/js/tailwind.js"></script>
+    <link href='https://www.wikipeoplestats.org/assets/css/fonts/styles.css' rel='stylesheet' type='text/css'>
+   <link rel="stylesheet" href="https://www.wikipeoplestats.org/libs/font-awesome/all.min.css">
+   <script src="https://www.wikipeoplestats.org/assets/js/tailwind.js"></script>
     <script>
         tailwind.config = {
             darkMode: 'class',
