a380bf40e6263151fb18de3ed25a08936e693f30
danielyepezgarces <danielyepezgarces@gmail.com>
2025-05-28 18:50:20 -0500

    fix layout issue

--- group.php
+++ group.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include 'languages.php'; // Cargar idiomas y traducciones
 
 // Definir base path dinámico para assets
